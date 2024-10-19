881d758dd56d567ed21393a2e8e125a7c42c3919
Dmitriy <dimaovch.04@mail.ru>
2024-10-19 21:57:38 +0300

    Добавление игровой механики

---
+++ src/js/index.js
@@ -0,0 +1,35 @@
+import GameLogic from './Logic/GameLogic.js';
+
+// Инициализируем игровую логику. Входной параметр - размер игрового поля
+const gameLogic = new GameLogic(3);
+
+// Получаем ячейки игрового поля
+var articles = $('#game__board td');
+
+// Добавление обработчиков события click
+$(document).ready(function () {
+
+	// Первый ход игрока
+	$('.fa-user-circle').click(function () {
+		newParty();
+	});
+
+	// Первый ход компьютера
+	$('.fa-desktop').click(function () {
+		newParty();
+		gameLogic.computer.randomStep($('.game > table  tr').length, gameLogic.gameModel, gameLogic.view);
+	});
+});
+
+// Запуск новой игровой партии
+function newParty() {
+
+	// Сброс UI игры в начало партии
+	gameLogic.view.restart($('#game__board'));
+
+	gameLogic.clearModel();
+	// Вешаем обработчик кликов на ячейки игрового поля
+	$(articles).click(function () {
+		gameLogic.clickOnCell(this);
+	});
+}
